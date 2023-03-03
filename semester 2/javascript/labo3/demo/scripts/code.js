f8b43aecb83c44148b0938ea5ff447c3e5295fd9
RobinVandenbroucke <115148304+RobinVandenbroucke78@users.noreply.github.com>
2023-03-03 10:54:25 +0100

    demo labo3

---
+++ semester 2/javascript/labo3/demo/scripts/code.js
@@ -0,0 +1,88 @@
+const setup = () => {
+    let btnTry = document.getElementById("btnTry");
+
+    btnTry.addEventListener("mouseover", mouseHover);
+    btnTry.addEventListener("click", onClick);
+    btnTry.addEventListener("mouseout", mouseOut);
+
+    //eventListeners CSS
+    document.getElementById("btnWithoutBullets").addEventListener("click", withoutBullets);
+
+    document.getElementById("btnWithBullets").addEventListener("click", withBullets);
+
+    //eventListeners difference between "textContent" and "innerHTML"
+
+    document.getElementById("btnContent").addEventListener("click", changeContent);
+}
+
+//mouseHoverFunction
+const mouseHover = () => {
+    document.getElementById("event").innerHTML += "Mouse Hovered!<br>";
+}
+
+//onClickFunction
+const onClick = () => {
+    document.getElementById("event").innerHTML += "Mouse Clicked!<br>";
+}
+
+//mouseOutFunction
+const mouseOut = () => {
+    document.getElementById("event").innerHTML += "Mouse Out!<br>";
+}
+
+//changeCSS
+const withoutBullets = () => {
+    let i;
+    let listItems = document.getElementsByTagName("li");
+    for (i = 0; i < listItems.length; i++){
+        //1ste manier (nooit gebruiken!!!)
+        //listItems[i].style.listStyleType="none";
+        //listItems[i].style.backgroundColor="red";
+
+        //2de manier
+        //listItems[i].className = "listItemsStyleNone colorRed"; //names see style.css
+
+        //3de manier
+        if (listItems[i].classList.contains("listItemsStyleDisc")){
+            listItems[i].classList.remove("listItemsStyleDisc");
+        }
+        listItems[i].classList.add("listItemsStyleNone");
+        if (listItems[i].classList.contains("colorWhite")){
+            listItems[i].classList.remove("colorWhite");
+        }
+        listItems[i].classList.add("colorRed");
+        console.log("output " + listItems[i].className);
+    }
+}
+
+const withBullets = () => {
+    let i;
+    let listItems = document.getElementsByTagName("li");
+    for (i = 0; i < listItems.length; i++){
+        //1ste manier (nooit gebruiken!!!)
+        //listItems[i].style.listStyleType="disc";
+        //listItems[i].style.backgroundColor="white";
+
+        //2de manier
+        //listItems[i].className = "listItemsStyleDisc colorWhite";
+
+        //3de manier
+        if (listItems[i].classList.contains("listItemsStyleNone")){
+            listItems[i].classList.remove("listItemsStyleNone");
+        }
+        listItems[i].classList.add("listItemsStyleDisc");
+        if (listItems[i].classList.contains("colorRed")){
+            listItems[i].classList.remove("colorRed");
+        }
+        listItems[i].classList.add("colorWhite");
+        console.log("output " + listItems[i].className);
+    }
+}
+
+// difference between "textContent" and "innerHTML"
+const changeContent = () => {
+    document.getElementById("textContent").textContent ="<a href='https://www.vives.be'>VIVES</a>";
+    document.getElementById("innerHTML").innerHTML ="<a href='https://www.vives.be'>VIVES</a>";
+}
+
+window.addEventListener("load", setup);
