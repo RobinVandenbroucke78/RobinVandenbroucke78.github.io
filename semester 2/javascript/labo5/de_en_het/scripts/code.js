77791f15fd1033a6eb2f3b15a1525ce42992b21c
RobinVandenbroucke <115148304+RobinVandenbroucke78@users.noreply.github.com>
2023-03-10 11:32:48 +0100

    labo6

--- semester 2/javascript/labo5/de_en_het/scripts/code.js
+++ semester 2/javascript/labo5/de_en_het/scripts/code.js
@@ -1,9 +1,15 @@
 const setup = () => {
     let tekst = "Gisteren zat de jongen op de stoep en at de helft van de appel";
-    let de = "de"
-    let het = "het"
-    let zin = tekst.trim().replace(new RegExp(de, "gi"), het); //reguliere expressie, gi = global ignore case
-    console.log(zin);
+    let de = "de";
+    let het = "het";
+    let zin = tekst.split(" ");
 
+    for(let i = 0; i < zin.length; i++) {
+        if(zin[i].toLowerCase() === de.toLowerCase()) {
+            zin[i] = het;
+        }
+    }
+    let nieuweZin = zin.join(" ");
+    console.log(nieuweZin);
 }
 window.addEventListener("load", setup);
